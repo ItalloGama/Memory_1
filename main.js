1c997f4cbe089d0390a1f3d399b6e7a1fc2ce163
ItalloGama <Itallo.gama15@gmail.com>
2021-08-10 16:30:39 -0400

    tiles

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
 let tiles = document.querySelectorAll('.tiles')
-console.log(tiles)
-let tileId0 = document.getElementById('0')
-let tileId1 = document.getElementById('1')
-let tileId2 = document.getElementById('2')
-let tileId3 = document.getElementById('3')
-let tileId4 = document.getElementById('4')
+for (let i = 0; i < tiles.length; i++) {
+  tiles[i].addEventListener('click', tileClick)
+}
 
 let gameSequence = document.getElementById('gameSequence')
+let turn = 0
+// console.log(tiles)
+const levelSequence1 = [0, 4, 0]
+const playerChoices = []
 
-
-const levelSequence1 = [1, 4, 0]
-const playerChoice = []
-
-function tileClick (event){
-tiles.addEventListener('click', () => {
-console.log(event.target.id)
-})
+function tileClick(event) {
+  let playerChoice = parseInt(event.target.id)
+  let match = checkMatch(playerChoice, levelSequence1[turn])
+  playerChoices.push(match)
+  console.log(`match; ${match}`)
+  console.log(`turn; ${turn}`)
+  console.log(`playerChoice; ${playerChoice}`)
+  turn++
+  if (match === false) {
+    gameLost()
+  }
 }
-// function tileChoice
+
+function gameLost() {
+  alert('You Lost!')
+  turn = 0
+  playerChoices = []
+}
+
+function checkWinner() {
+  if (playerChoices.length === levelSequence1.length) {
+    alert('we have a winner')
+  }
+}
+function checkMatch(playerChoice, sequesenceValue) {
+  if (playerChoice === sequesenceValue) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// tileId1.addEventListener('click', (event) => {
+//     console.log(parseInt(event.target.id))
+//     playerChoice.push(parseInt(event.target.id))
+// })
+// tileId2.addEventListener('click', (event) => {
+//     console.log(parseInt(event.target.id))
+//     playerChoice.push(parseInt(event.target.id))
+// })
+// tileId3.addEventListener('click', (event) => {
+//     console.log(parseInt(event.target.id))
+//     playerChoice.push(parseInt(event.target.id))
+// })
+// tileId4.addEventListener('click', (event) => {
+//     console.log(parseInt(event.target.id))
+//     playerChoice.push(parseInt(event.target.id))
+// })

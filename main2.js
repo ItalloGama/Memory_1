ce6db9775c4340b893449cebc4ae19c93eb77717
ItalloGama <Itallo.gama15@gmail.com>
2021-08-11 13:36:29 -0400

    lvl1 and home page mvp

---
+++ main2.js
@@ -0,0 +1,50 @@
+let tiles = document.querySelectorAll('.tiles')
+for (let i = 0; i < tiles.length; i++) {
+  tiles[i].addEventListener('click', tileClick)
+}
+
+
+setTimeout(function(){
+    let gameSequence = document.getElementById('gameSequence')
+    gameSequence.parentNode.removeChild(gameSequence)
+}, 4000);
+let turn = 0
+// console.log(tiles)
+const levelSequence2 = [0, 4, 0, 3, 2]
+let playerChoices = []
+
+function tileClick(event) {
+  let playerChoice = parseInt(event.target.id)
+  let match = checkMatch(playerChoice, levelSequence2[turn])
+  playerChoices.push(match)
+  console.log(`match; ${match}`)
+  console.log(`turn; ${turn}`)
+  console.log(`playerChoice; ${playerChoice}`)
+  turn++
+  checkWinner()
+  if (match === false) {
+    gameLost()
+  }
+}
+
+function gameLost() {
+  alert('You Lost!')
+  turn = 0
+  playerChoices = []
+}
+
+function checkWinner() {
+  if (playerChoices.length === levelSequence2.length) {
+    alert('we have a winner')
+    turn = 0
+    playerChoices = []
+  }
+}
+function checkMatch(playerChoice, sequesenceValue) {
+  if (playerChoice === sequesenceValue) {
+    return true
+  } else {
+    return false
+  }
+}
+
---
+++ main3.js
@@ -0,0 +1,50 @@
+let tiles = document.querySelectorAll('.tiles')
+for (let i = 0; i < tiles.length; i++) {
+  tiles[i].addEventListener('click', tileClick)
+}
+
+
+setTimeout(function(){
+    let gameSequence = document.getElementById('gameSequence')
+    gameSequence.parentNode.removeChild(gameSequence)
+}, 4000);
+let turn = 0
+// console.log(tiles)
+const levelSequence3 = [0, 4, 0, 3, 2, 1, 1, 4, 0]
+let playerChoices = []
+
+function tileClick(event) {
+  let playerChoice = parseInt(event.target.id)
+  let match = checkMatch(playerChoice, levelSequence3[turn])
+  playerChoices.push(match)
+  console.log(`match; ${match}`)
+  console.log(`turn; ${turn}`)
+  console.log(`playerChoice; ${playerChoice}`)
+  turn++
+  checkWinner()
+  if (match === false) {
+    gameLost()
+  }
+}
+
+function gameLost() {
+  alert('You Lost!')
+  turn = 0
+  playerChoices = []
+}
+
+function checkWinner() {
+  if (playerChoices.length === levelSequence3.length) {
+    alert('we have a winner')
+    turn = 0
+    playerChoices = []
+  }
+}
+function checkMatch(playerChoice, sequesenceValue) {
+  if (playerChoice === sequesenceValue) {
+    return true
+  } else {
+    return false
+  }     
+}
+
